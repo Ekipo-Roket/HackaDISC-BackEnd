626b5b5a56bbae1b5ad3e6e26e5965d7ef2b60f8
nato <renato.morales@alumnos.ucn.cl>
2024-06-20 10:46:43 -0400

    feat: getEvaluationsByWorkers

--- app/Http/Controllers/API/Evaluation/EvaluationsController.php
+++ app/Http/Controllers/API/Evaluation/EvaluationsController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\API\Evaluation;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Evaluation;
 
 class EvaluationsController extends Controller
 {
-    //
+    public function getEvaluationsByWorker($worker_id){
+        try{
+            $evaluations = Evaluation::where('user_id', $worker_id)->get();
+
+            return response()->json($evaluations);
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => $e->getMessage(),s
+            ], 500);
+        }
+    }
 }
