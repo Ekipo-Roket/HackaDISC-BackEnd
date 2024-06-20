883c43775adffbf6da626a101c926a2cb8de8a6b
nato <renato.morales@alumnos.ucn.cl>
2024-06-20 09:52:47 -0400

    feat: Evaluations y Workers controllers

--- app/Http/Controllers/API/Workers/WorkersController.php
+++ app/Http/Controllers/API/Workers/WorkersController.php
@@ -10,7 +10,7 @@ class WorkersController extends Controller
 {
     public function getAllWorkers(){
         try{
-            $workers = Workers::get();
+            $workers = Worker::get();
             return response()->json($workers);
         }catch(\Exception $e){
             return response()->json([
@@ -20,7 +20,7 @@ class WorkersController extends Controller
     }
     public function getWorkersByCompany($company_id){
         try{
-            $workers = Workers::where('company_id', $company_id)->get();
+            $workers = Worker::where('company_id', $company_id)->get();
             return response()->json($workers);
         }catch(\Exception $e){
             return response()->json([
@@ -33,6 +33,16 @@ class WorkersController extends Controller
         try{
             $worker = Worker::find($id);
             return response()->json($worker);
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+    public function getWorkersByArea($area_id){
+        try{
+            $workers = Worker::where('area_id', $area_id)->get();
+            return response()->json($workers);
         }catch(\Exception $e){
             return response()->json([
                 'message' => $e->getMessage(),
